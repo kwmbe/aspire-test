71f4c488ecfb9fd163a50717fd05256fbf8b001a
kwmbe <koberkein@gmail.com>
2026-02-21 23:09:14 +0100

    Add database

--- Aspire/AppHost.cs
+++ Aspire/AppHost.cs
@@ -2,6 +2,11 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddSqlServer("sql")
+    .WithDataVolume("data")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .AddDatabase("sqldb");
+
 builder.AddProject<Tester>("tester");
 
 builder.Build().Run();
